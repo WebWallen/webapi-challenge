46b11f6883887015c506707e5cb8bba61e957d99
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-04 11:45:53 -0400

    Server rending on .get as expected but doesn't like routers yet.

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const helmet = require('helmet');
 
 // Put routers here
+const projectRoutes = require('./routes/projectRoutes');
+const actionRoutes = require('./routes/actionRoutes');
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 
 // Put routes here
+// server.use('/projects', projectRoutes);
+// server.use('/actions', actionRoutes);
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Testing 1 2 3 </h1>`)
+})
 
 server.listen(4000, () => console.log('API running on port 4000'))
